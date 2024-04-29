cea806f9188661436e192f663c4d341108f0ecce
nflsdq <168415198+nflsdq@users.noreply.github.com>
2024-04-29 16:26:43 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -19,10 +19,10 @@ function faktor(n) {
 }
 
 function cekRelatifPrima() {
-    const num1 = document.getElementById('num1').value;
-    const num2 = document.getElementById('num2').value;
+    const bil1 = document.getElementById('bil1').value;
+    const bil2 = document.getElementById('bil2').value;
 
-    if (num1 === '' || num2 === '') {
+    if (bil1 === '' || bil2 === '') {
         document.getElementById('hasil').style.display = 'block';
         document.getElementById('hasil').innerHTML = 'Silakan masukkan kedua bilangan.';
         document.getElementById('hasil').style.borderColor = '#f44336';
@@ -30,35 +30,35 @@ function cekRelatifPrima() {
         return;
     }
 
-    const num1Int = parseInt(num1);
-    const num2Int = parseInt(num2);
-    const ppb = hitungPPB(num1Int, num2Int);
-    const faktor1 = faktor(num1Int);
-    const faktor2 = faktor(num2Int);
+    const bil1Int = parseInt(bil1);
+    const bil2Int = parseInt(bil2);
+    const ppb = hitungPPB(bil1Int, bil2Int);
+    const faktor1 = faktor(bil1Int);
+    const faktor2 = faktor(bil2Int);
 
     let faktor1Text = faktor1.map(f => faktor2.includes(f) ? `<strong>${f}</strong>` : f).join(', ');
     let faktor2Text = faktor2.map(f => faktor1.includes(f) ? `<strong>${f}</strong>` : f).join(', ');
 
     document.getElementById('faktor').style.display = 'block';
-    document.getElementById('faktor').innerHTML = `Faktor ${num1Int}: ${faktor1Text} <br> Faktor ${num2Int}: ${faktor2Text}`;
+    document.getElementById('faktor').innerHTML = `Faktor ${bil1Int}: ${faktor1Text} <br> Faktor ${bil2Int}: ${faktor2Text}`;
 
     document.getElementById('hasil').style.display = 'block';
     if (ppb === 1) {
-        document.getElementById('hasil').innerHTML = `Bilangan ${num1Int} dan ${num2Int} adalah relatif prima.`;
+        document.getElementById('hasil').innerHTML = `Bilangan ${bil1Int} dan ${bil2Int} adalah relatif prima.`;
         document.getElementById('hasil').style.borderColor = '#4CAF50';
     } else {
-        document.getElementById('hasil').innerHTML = `Bilangan ${num1Int} dan ${num2Int} bukan relatif prima karena PPBnya adalah ${ppb}.`;
+        document.getElementById('hasil').innerHTML = `Bilangan ${bil1Int} dan ${bil2Int} bukan relatif prima karena PPBnya adalah ${ppb}.`;
         document.getElementById('hasil').style.borderColor = '#f44336';
     }
 }
 
-document.getElementById('num1').addEventListener('keyup', function(event) {
+document.getElementById('bil1').addEventListener('keyup', function(event) {
     if (event.keyCode === 13) {
         cekRelatifPrima();
     }
 });
 
-document.getElementById('num2').addEventListener('keyup', function(event) {
+document.getElementById('bil2').addEventListener('keyup', function(event) {
     if (event.keyCode === 13) {
         cekRelatifPrima();
     }
